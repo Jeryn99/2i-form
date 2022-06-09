0818b8b943ec4b79ebc09a12cc46906313688e0c
Craig <sandedshoes@gmail.com>
2022-06-10 00:17:37 +0100

    New test data + Form adjustments
    - Age Field
    - County Field
    - Language Field

--- cypress/e2e/form_test.cy.js
+++ cypress/e2e/form_test.cy.js
@@ -1,5 +1,24 @@
 import data from '../fixtures/people.json'
 
+function csvJSON(csv){
+  var lines=csv.toString().split("\n");
+  var result = [];
+  var headers=lines[0].split(",");
+
+  for(var i=1;i<lines.length;i++){
+
+      var obj = {};
+      var currentline=lines[i].split(",");
+
+      for(var j=0;j<headers.length;j++){
+          obj[headers[j]] = currentline[j];
+      }
+
+      result.push(obj);
+  }
+  return JSON.parse(JSON.stringify(result)); 
+}
+
 describe('Navigate to Web Application', () => {
   it('visits application page', () => {
     cy.visit('http://localhost:3000/')
@@ -13,10 +32,13 @@ describe('Tests Valid User Input', () => {
 
   data.forEach(item => {
     console.log(item)
-    it(`Tests Data for ${item.firstName} ${item.lastName}`, () => {
-      cy.get('input[id="formGridFirstName"]').type(item.firstName);
-      cy.get('input[id="formGridLastName"]').type(item.lastName);
+    it(`Tests Data for ${item.first_name} ${item.last_name}`, () => {
+      cy.get('input[id="formGridFirstName"]').type(item.first_name);
+      cy.get('input[id="formGridLastName"]').type(item.last_name);
+      cy.get('input[id="formGridAge"]').type(item.age);
       cy.get('input[id="formGridEmail"]').type(item.email);
+      cy.get('input[id="formGridPhone"]').type(item.phone_number);
+
       
       cy.get('button[type="submit"]').click()
     })
--- src/App.js
+++ src/App.js
@@ -36,6 +36,11 @@ function App() {
               <Form.Control placeholder="Doe" />
             </Form.Group>
 
+            <Form.Group as={Col} controlId="formGridAge">
+              <Form.Label>Age</Form.Label>
+              <Form.Control type="number" placeholder="22" min="18" max="100" id="age" name="age"/>
+            </Form.Group>
+
           </Row>
 
           <Row className="mb-3">
@@ -47,13 +52,23 @@ function App() {
           </Row>
 
           <Row className="mb-3">
-            <Form.Group controlId="formGridAddress">
+          <Form.Group controlId="formGridAddress">
               <Form.Label>Address</Form.Label>
               <Form.Control placeholder="46 Dalhousie Road" />
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridZip">
+            <Form.Group controlId="formGridAddress2">
+              <Form.Label>County</Form.Label>
+              <Form.Control placeholder="Midlothian" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPostcode">
               <Form.Label>Postcode</Form.Label>
+              <Form.Control placeholder="EH22 3FR" title="Please enter a valid UK postcode" pattern="([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPhone">
+              <Form.Label>Phone Number</Form.Label>
               <Form.Control placeholder="EH22 3FR" title="Please enter a valid UK postcode" pattern="([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})" />
             </Form.Group>
           </Row>
