80e0c29d6dd0ba6a3418a91bf95b44785591af91
Craig <sandedshoes@gmail.com>
2022-06-15 12:07:37 +0100

    Began invalid data testing

--- cypress/e2e/form_test.cy.js
+++ cypress/e2e/form_test.cy.js
@@ -1,42 +1,22 @@
 import data from '../fixtures/people.json'
-
-function csvJSON(csv) {
-  var lines = csv.toString().split("\n");
-  var result = [];
-  var headers = lines[0].split(",");
-
-  for (var i = 1; i < lines.length; i++) {
-
-    var obj = {};
-    var currentline = lines[i].split(",");
-
-    for (var j = 0; j < headers.length; j++) {
-      obj[headers[j]] = currentline[j];
-    }
-
-    result.push(obj);
-  }
-  return JSON.parse(JSON.stringify(result));
-}
+import invalidUser from '../fixtures/invalid_user.json'
 
 describe('Navigate to Web Application', () => {
-  it('visits application page', () => {
+  it('Visits application page', () => {
     cy.visit('http://localhost:3000/')
-    cy.contains('Form').should("exist")
+    cy.contains('Registration').should("exist")
   })
 })
-
-
 
 describe('Tests Valid User Input', () => {
 
   data.forEach(user => {
 
     it(`Tests Data for ${user.first_name} ${user.last_name}`, () => {
-      cy.reload(true)
+      // cy.reload(true)
       cy.get('input[id="formGridFirstName"]').type(user.first_name).should('have.value', user.first_name);
       cy.get('input[id="formGridLastName"]').type(user.last_name).should('have.value', user.last_name);
-      cy.get('input[id="formGridAge"]').type(user.age).should('have.value', user.age);
+      cy.get('input[id="formGridAge"]').clear().type(user.age).should('have.value', user.age);
       cy.get('input[id="formGridEmail"]').type(user.email).should('have.value', user.email);
       cy.get('input[id="formGridPhone"]').type(user.phone_number).should('have.value', user.phone_number);
 
@@ -45,17 +25,39 @@ describe('Tests Valid User Input', () => {
       cy.get('input[id="formGridAddress"]').type(address[0] + address[1]).should('have.value', address[0] + address[1]);
       cy.get('input[id="formGridAddress2"]').type(address[2].trim()).should('have.value', address[2].trim());
       cy.get('input[id="formGridPostcode"]').type(address[3].trim()).should('have.value', address[3].trim());
-
       cy.get('select[id="formGridLanguage"]').select(user.language).should('have.value', user.language);
 
       var isCarUser = user.hasOwnProperty('car');
       cy.get('select[id="formGridType"]').select(isCarUser ? "Car" : "Van");
       cy.get('select[id="formGridVehicle"]').select(isCarUser ? user.car : user.van);
+      cy.contains(isCarUser ? "Car Model" : "Van Model").should("exist")
+      cy.contains(isCarUser ? "Van Model" : "Car Model").should("not.exist")
 
+      cy.get('button[name="submit"]').click()
 
-      cy.get('button[type="submit"]')
+      cy.contains('Successfully Registered').should("exist")
+      cy.get('button[name="close"]').click()
+
     })
   })
-
 })
 
+
+describe('Tests Invalid Data reactions', () => {
+
+
+
+  it(`Populates Data for ${invalidUser.first_name} ${invalidUser.last_name}`, () => {
+    cy.get('input[id="formGridFirstName"]').type(invalidUser.first_name).should('have.value', invalidUser.first_name);
+    cy.get('input[id="formGridLastName"]').type(invalidUser.last_name).should('have.value', invalidUser.last_name);
+  });
+
+  it(`Tests invalid age for ${invalidUser.first_name} ${invalidUser.last_name}`, () => {
+    cy.get('input[id="formGridAge"]').clear().type(invalidUser.invalid_age).should('have.value', invalidUser.invalid_age);
+    cy.get('button[name="submit"]').click()
+    cy.contains("Please check your inputs for errors.");
+    cy.get('input[id="formGridAge"]').clear().type(invalidUser.valid_age).should('have.value', invalidUser.valid_age);
+  });
+
+
+});
--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import vans from './data/vans.json';
 import languages from './data/languages.json';
 
 import { useState } from 'react';
-import { Button, Row, Col, Form } from 'react-bootstrap';
+import { Button, Row, Col, Form, Modal, Alert, Toast , ToastContainer} from 'react-bootstrap';
 
 const carJson = JSON.parse(JSON.stringify(cars));
 const vanJson = JSON.parse(JSON.stringify(vans));
 const languagesJson = JSON.parse(JSON.stringify(languages));
-
 
 function jsonToOptions(json) {
   let options = [];
@@ -23,121 +22,208 @@ function jsonToOptions(json) {
 
 function App() {
 
-  const [carUser, setCarUser] = useState(false);
+  // Modal
+  const [showModal, setShowModal] = useState(false);
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  // User Data
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState(22);
+  const [addressLine, setAddressLine] = useState('');
+  const [county, setCounty] = useState('');
+  const [postcode, setPostcode] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [useCar, setUsesCar] = useState(null);
+  const [vehicleModel, setVehicleModel] = useState('');
+  const [language, setLanguage] = useState('');
+
+  // Error Handling
+  const [showError, setShow] = useState(false);
+
+  function onHandleFormSubmit(e) {
+    e.preventDefault();
+    handleShow();
+    e.target.reset();
+  }
+
+  function closeAndClearData() {
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setAge(0);
+    setAddressLine("");
+    setCounty("");
+    setPostcode("");
+    setPhoneNumber("");
+    setUsesCar(true);
+    setVehicleModel("");
+    setLanguage("");
+    handleClose();
+  }
 
   return (
 
     <div className="App">
-        <h1>Registration</h1>
-          <Form>
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridFirstName">
-                <Form.Label>First name</Form.Label>
-                <Form.Control placeholder="John" />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridLastName">
-                <Form.Label>Last name</Form.Label>
-                <Form.Control placeholder="Doe" />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridAge">
-                <Form.Label>Age</Form.Label>
-                <Form.Control type="number" placeholder="22" min="18" max="100" />
-              </Form.Group>
-            </Row>
 
 
-            <Row className="mb-6">
+      <Alert variant="danger" show={showError} onClose={() => setShow(false)}>
+        <Alert.Heading>Invalid Input Detected!</Alert.Heading>
+        {
+          <p>Please check your inputs for errors.</p>
+        }
+      </Alert>
+      <h1>Registration</h1>
 
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="schools@edinburghcollege.ac.uk" title="Please give a valid email address" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+      <Form onSubmit={onHandleFormSubmit}>
 
-              <Form.Group as={Col} controlId="formGridLanguage">
-                <Form.Label>Language</Form.Label>
-                <Form.Select defaultValue="Choose...">
-                  {jsonToOptions(languagesJson).map(item => (
-                    <option key={item.value} value={item.value}>{item.label}</option>
-                  ))}
-                </Form.Select>
-                <Form.Text className="text-muted">
-                  Please select your choosen language
-                </Form.Text>
-              </Form.Group>
-
-            </Row>
-
-            <Row className="mb-6">
-              <Form.Group controlId="formGridAddress">
-                <Form.Label>Address</Form.Label>
-                <Form.Control placeholder="46 Dalhousie Road" />
-                <Form.Text className="text-muted">
-                  First line of your address
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group controlId="formGridAddress2">
-                <Form.Label>County</Form.Label>
-                <Form.Control placeholder="Midlothian" />
-              </Form.Group>
-            </Row>
-
-            <Row className="mb-6">
-              <Form.Group as={Col} controlId="formGridPostcode">
-                <Form.Label>Postcode</Form.Label>
-                <Form.Control placeholder="EH22 3FR" title="Please enter a valid UK postcode" pattern="([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})" />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPhone">
-                <Form.Label>Phone Number</Form.Label>
-                <Form.Control placeholder="0131 297 9000" />
-              </Form.Group>
-
-            </Row>
+        <Row className="mb-3">
 
 
+          <Form.Group as={Col} controlId="formGridFirstName">
+            <Form.Label>First name</Form.Label>
+            <Form.Control placeholder="John" value={firstName} onChange={e => { setFirstName(e.target.value) }} />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridLastName">
+            <Form.Label>Last name</Form.Label>
+            <Form.Control placeholder="Doe" value={lastName} onChange={e => { setLastName(e.target.value) }} />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridAge">
+            <Form.Label>Age</Form.Label>
+            <Form.Control type="number" placeholder="22" min="18" max="100" value={age} onChange={e => {
+              setShow(e.target.value < 18 || e.target.value > 100);
+              setAge(e.target.value)
+            }} />
+          </Form.Group>
+        </Row>
 
 
-            <Row className="mb-3">
+        <Row className="mb-6">
 
-              <Form.Group as={Col} controlId="formGridType">
-                <Form.Label>Vehicle Type</Form.Label>
-                <Form.Select defaultValue="Choose..." onChange={e => {
-                  setCarUser(e.target.value === "Car");
-                }}>
-                  <option>Van</option>
-                  <option>Car</option>
-                </Form.Select>
-                <Form.Text className="text-muted">
-                  Please select the relevant vehicle type
-                </Form.Text>
-              </Form.Group>
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="schools@edinburghcollege.ac.uk" title="Please give a valid email address" value={email} onChange={e => {
+              setEmail(e.target.value);
+            }} />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-              <Form.Group as={Col} controlId="formGridVehicle">
-                <Form.Label>{carUser ? "Car Model" : "Van Model"}</Form.Label>
-                <Form.Select defaultValue="Choose...">
+          <Form.Group as={Col} controlId="formGridLanguage" value={language} onChange={e => { setLanguage(e.target.value) }}>
+            <Form.Label>Language</Form.Label>
+            <Form.Select defaultValue="Choose...">
+              {jsonToOptions(languagesJson).map(item => (
+                <option key={item.value} value={item.value}>{item.label}</option>
+              ))}
+            </Form.Select>
+            <Form.Text className="text-muted">
+              Please select your choosen language
+            </Form.Text>
+          </Form.Group>
 
-                  {jsonToOptions(carUser ? carJson : vanJson).map(item => (
-                    <option key={item.value} value={item.value}>{item.label}</option>
-                  ))}
+        </Row>
 
-                </Form.Select>
-                <Form.Text className="text-muted">
-                  Please select the relevant vehicle model
-                </Form.Text>
-              </Form.Group>
-            </Row>
+        <Row className="mb-6">
+          <Form.Group controlId="formGridAddress" value={addressLine} onChange={e => { setAddressLine(e.target.value) }}>
+            <Form.Label>Address</Form.Label>
+            <Form.Control placeholder="46 Dalhousie Road" />
+            <Form.Text className="text-muted">
+              First line of your address
+            </Form.Text>
+          </Form.Group>
 
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-    </div>
+          <Form.Group controlId="formGridAddress2" value={county} onChange={e => { setCounty(e.target.value) }}>
+            <Form.Label>County</Form.Label>
+            <Form.Control placeholder="Midlothian" />
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-6">
+          <Form.Group as={Col} controlId="formGridPostcode" value={postcode} onChange={e => { setPostcode(e.target.value) }}>
+            <Form.Label>Postcode</Form.Label>
+            <Form.Control placeholder="EH22 3FR" title="Please enter a valid UK postcode" pattern="([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridPhone" value={phoneNumber} onChange={e => { setPhoneNumber(e.target.value) }}>
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control placeholder="0131 297 9000" />
+          </Form.Group>
+
+        </Row>
+
+
+
+
+        <Row className="mb-3">
+
+          <Form.Group as={Col} controlId="formGridType">
+            <Form.Label>Vehicle Type</Form.Label>
+            <Form.Select defaultValue="Choose..." onChange={e => {
+              setUsesCar(e.target.value === "Car");
+            }}>
+              <option>Van</option>
+              <option>Car</option>
+            </Form.Select>
+            <Form.Text className="text-muted">
+              Please select the relevant vehicle type
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridVehicle" value={vehicleModel} onChange={e => { setVehicleModel(e.target.value) }}>
+            <Form.Label>{useCar ? "Car Model" : "Van Model"}</Form.Label>
+            <Form.Select defaultValue="Choose...">
+
+              {jsonToOptions(useCar ? carJson : vanJson).map(item => (
+                <option key={item.value} value={item.value}>{item.label}</option>
+              ))}
+
+            </Form.Select>
+            <Form.Text className="text-muted">
+              Please select the relevant vehicle model
+            </Form.Text>
+          </Form.Group>
+        </Row>
+
+        <Button variant="primary" name="submit" type="submit">
+          Submit
+        </Button>
+      </Form>
+
+
+      <Modal
+        show={showModal}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Successfully Registered! ({firstName + " " + lastName})</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Name: {firstName + " " + lastName}</p>
+          <p>Age: {age}</p>
+          <p>Email: {email}</p>
+          <p>Language: {language}</p>
+          <p>Address: {addressLine}</p>
+          <p>County: {county}</p>
+          <p>Postcode: {postcode}</p>
+          <p>Phone Number: {phoneNumber}</p>
+          <p>Vehicle Type: {useCar ? "Car" : "Van"}</p>
+          <p>Vehicle Model: {vehicleModel}</p>
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" name='close' onClick={closeAndClearData}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+    </div >
   );
 }
 
